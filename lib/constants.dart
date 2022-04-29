9435dcc537b935bcde5279dd321faa6fa5e9a661
abhavgoel <goelabhav2002@gmail.com>
2022-04-30 04:18:54 +0530

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+library constants;
+
+import 'package:flutter/material.dart';
+
+const PRIMARY_COLOR = Color.fromARGB(255, 142, 255, 142);
+const ACCENT_COLOR = MaterialColor(0xff314675, <int, Color>{});
+
+const DATABASE_NAME = 'helm';
