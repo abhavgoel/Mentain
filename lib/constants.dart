d80ea50638830ec321a9384993fa25ff3824a19c
abhavgoel <goelabhav2002@gmail.com>
2022-04-30 12:33:43 +0530

    color change

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,7 +2,7 @@ library constants;
 
 import 'package:flutter/material.dart';
 
-const PRIMARY_COLOR = Color.fromARGB(255, 142, 255, 142);
+const PRIMARY_COLOR = Color.fromARGB(255, 244, 118, 185);
 const ACCENT_COLOR = MaterialColor(0xff314675, <int, Color>{});
 
-const DATABASE_NAME = 'helm';
+const DATABASE_NAME = 'mentain';
